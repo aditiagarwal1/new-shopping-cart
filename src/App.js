d9b4562e445e65ab9024b38934ebf7d1e161e8ec
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 12:47:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <MultProducts products={info.products} />
+        <Product products={info.products} />
       </div>
     );
   }
