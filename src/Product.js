a73f15f4e8617783cd31f64e0c01966d980db98e
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 10:10:32 -0600

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.scss';
 
@@ -69,17 +69,19 @@ export default Product;
 
 class Product extends Component {
   render() {
+    const item_name = this.props.id;
   
 return (
   <div className = "shelf-item">
-  <img className = "shelf-item__thumb" src={require(`./static/data/products/${product.sku}_1.jpg`)} alt={product.title} /> 
-  <p className="shelf-item__title">{product.title}</p>
- <Button className="shelf-item__buy-btn">Add to cart</Button>
+  <img className = "shelf-item__thumb" src={require(`./static/data/products/${this.props.item.sku}_1.jpg`)} alt={this.props.item.title} /> 
+  <p className="shelf-item__title">{this.propsitem.title}</p>
+ <div className="shelf-item__buy-btn">Add to cart</div>
 </div>
 )
 }
 }
-export default Product;
+export default 
+;
 
 /*
     <div
