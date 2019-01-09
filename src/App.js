c1308933577c5e5436e9ae88c35e0961e648be1b
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 00:31:37 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import './product.js'
+import './product.js';
 /*
 class App extends Component {
   render() {
