0b224b83f093399ebe70ba79bbc666ae65917fc7
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 10:40:44 -0600

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { Row, Col, Button } from 'react-bootstrap'
 import './App.scss';
+import data from './static/data/products/products.json'
 
+/*
 import Thumb from './Thumb';
 
 const Product = props => {
@@ -23,4 +25,17 @@ const Product = props => {
 }
 
 
+export default Product;
+*/
+
+class Product extends Component {
+  render() {
+    return (
+        <img src={require(`./static/data/products/${this.props.product.sku}_1.jpg`)} alt="this.props.product.title"/>
+        <p> {this.props.product.title} </p>
+        <p> {`${this.props.product.price}`} </p>
+        <Button> Add Item to cart </Button>
+    )
+  }
+}
 export default Product;
