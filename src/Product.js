52a3a8ca6aebc0ea52885e4c3d2714df3920974e
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 09:56:59 -0600

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -80,7 +80,7 @@ return (
       )}
       <Thumb
         classes="shelf-item__thumb"
-       src={require(`../../../static/products/${product.sku}_1.jpg`)}
+       src={require(`./static/data/products/${product.sku}_1.jpg`)}
         alt={product.title}
       /> 
       <p className="shelf-item__title">{product.title}</p>
