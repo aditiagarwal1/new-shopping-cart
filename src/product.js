b080bae99a0127e8aad0aa5c8b7bf20e3b29bce8
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-09 00:22:11 -0600

    Update product.js

--- src/product.js
+++ src/product.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
 import './product.css';
+
+class Product extends Component {
+  render() {
+    return (
+      <div className="App-header">
+      <p> hello </p>
+      </div>
+       );
+  }
+}
