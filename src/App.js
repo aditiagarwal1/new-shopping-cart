4819be310f559a889ccfefe951c685a95ee1d1c1
Aditi Agarwal <aditiagarwal2020@u.northwestern.edu>
2019-01-10 00:00:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import { Row } from 'react-bootstrap';
 
 function ProductShirt(props) {
   return <div className="prod">
-    {
-     props.products.map( product => <ProductShirtOne product={product} />)
-    }
+    {props.products.map( product => <ProductShirtOne product={product} />)}
   </div>
 }
 
@@ -19,7 +17,7 @@ function ProductShirtOne(props) {
     <img
 	height={300}
 	width={200}
-        title={ props.product.title }
+        title={props.product.title}
         src = {require(`./static/data/products/${props.product.sku}_1.jpg`)}
 />
 <p className="center">{props.product.title}</p>
